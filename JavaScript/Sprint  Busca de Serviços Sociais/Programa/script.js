4c81f83b806553e22ada441bb06ff24dd887bf9f
Silpharny <silpharny@gmail.com>
2024-04-28 16:47:22 -0300

    Finalizando a sprint

--- JavaScript/Sprint  Busca de Serviços Sociais/Programa/script.js
+++ JavaScript/Sprint  Busca de Serviços Sociais/Programa/script.js
@@ -20,41 +20,41 @@ const services = [
         contact: "aai@gmail.com"  
 }];
 
-function Services() {
+function SearchServices(region, type) {  
 
-    function SearchServices(region, type) {  
-        const results = services.filter(service => {    
-            return service.region === region && service.type === type;
-        });  
+    region = document.querySelector("#regionSearch").value
+    type = document.querySelector("#typeSearch").value
 
-        if (results.length === 0) {
-            console.log(`Não foram encontrados serviços sociais na região ${region} do tipo ${type}.`);
-        } else {
-            console.log(`Os seguintes serviços sociais foram encontrados na região ${region} do tipo ${type}:`);
-            results.forEach(result => {
-                console.log(`${result.name} - Contato: ${result.contact}`);    
-            });  
-        }
+    let respostaService = document.querySelector("#respostaService")
+
+    const results = services.filter(service => {    
+        return service.region === region && service.type === type;
+        
+    });  
+
+    if (results.length === 0) {
+        respostaService.textContent = `Não foram encontrados serviços sociais na região ${region} do tipo ${type}.`
+    } else {
+        respostaService.textContent = `Os seguintes serviços sociais foram encontrados na região ${region} do tipo ${type}:`
+        results.forEach(result => {
+            respostaService.textContent = `${result.name} - Contato: ${result.contact}`
+    });  
     }
-
-
-
-    function AddServices() {
-
-        let addName = document.querySelector("#name").value
-        let addRegion = document.querySelector("#region").value
-        let addType = document.querySelector("#type").value
-        let addContact = document.querySelector("#contact").value
-
-        let newService = {name: addName, region: addRegion, type: addType, contact: addContact}
-    
-        services.push(newService)
-
-    }
-    SearchServices("Zona Sul", "Apoio ao Idoso")
 }
 
+function AddServices() {
+
+    let addName = document.querySelector("#name").value
+    let addRegion = document.querySelector("#region").value
+    let addType = document.querySelector("#type").value
+    let addContact = document.querySelector("#contact").value
+
+    let newService = {name: addName, region: addRegion, type: addType, contact: addContact}
+
+    services.push(newService)
+
+}
+console.log(services);
 
 
 
-console.log(Services());
