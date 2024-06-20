6d7fb3e8afa71c7e89c257396152d5600330d9c1
Silpharny <silpharny@gmail.com>
2024-06-20 14:24:10 -0300

    Javascript Básico

---
+++ JavascriptBasico/array.js
@@ -0,0 +1,39 @@
+let listaFrutas = ['banana', 'maçã', 'abacate', 'abacaxi', 'morango']
+
+// Pegar a lista completa
+listaFrutas
+
+// Pegar item pelo índice -> [i] - Retorna o elemento de um índice específico
+listaFrutas[0]
+
+// Saber o tamanho da lista -> length - Retorna a quantidade de itens que tem na lista
+listaFrutas.length
+
+
+/* -------------------------------------------MANIPULAÇÃO DE ARRAYS------------------------------------------- */
+
+
+let userList = ['Arnaldo', 'Maria', 'Clara', 'Matheus', 'Sil', 'Gabriel', 'Lucas', 'Carlos', 'Mariana']
+
+// Pegar item específico da lista -> indexOf - Retorna o índice do elemento que estou pesquisando
+userList.indexOf('Sil') //índice 4 
+
+// Quando eu tento pegar um item que não existe na minha lista, o retorno vai ser -1
+// Exemplo: console.log(userList.indexOf('Sil213'))
+
+
+// Para alterar elemento de nossa lista
+userList[2] = 'Juliana' // Isso vai fazer com que o usuário do índice 2(que é a Clara) seja substituida por Juliana
+
+// push -> Adicionar item a lista
+userList.push('Laurinano')
+
+// shift -> Para remover o primeiro item do array
+userList.shift()
+
+// pop -> Para remover o último item
+userList.pop()
+
+// join -> Para retornar todos os valores do array separados por algum elemento
+userList.join(';')
+// No exemplo acima eu estou pedindo para imprimir minha lista separado por ';'
