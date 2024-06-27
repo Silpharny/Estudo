c0c8b1f1991217f0d55971bbc3768778a4ad8aad
Silpharny <silpharny@gmail.com>
2024-06-27 19:06:51 -0300

    Testes e desafios React Native

--- SujProgr/ReactNative/funcionalidadeNative/App.js
+++ SujProgr/ReactNative/funcionalidadeNative/App.js
@@ -1,48 +1,21 @@
+import "react-native-gesture-handler";
 import React, { createElement } from "react";
 import { NavigationContainer } from "@react-navigation/native"; // Importação do container que vai revestir as rotas
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer"; // Importação do drawer
 
-import { Feather } from "@expo/vector-icons"; // Importando os icones
+const Drawer = createDrawerNavigator();
 
 // Importação das páginas
 import Home from "./src/pages/Home";
 import Sobre from "./src/pages/Sobre";
 
-const Tab = createBottomTabNavigator(); // variavel para dar referencia ao tabBar
-
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        {" "}
-        {/* Criar uma ativar a tabBar */}
-        <Tab.Screen // Criar rota de navegação
-          name="Home" // Dar o nome para rota
-          component={Home} // Referenciar a página da rota
-          options={{
-            // Estilizar navBar
-            // Adicionando icone a tabBar
-            tabBarIcon: ({ color, size }) => {
-              return (
-                <Feather
-                  name="home"
-                  color={color}
-                  size={size} // Criando uma função anônima que vai receber o color e size padrão do icone, ele vai retornar um icone
-                />
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="Sobre"
-          component={Sobre}
-          options={{
-            tabBarIcon: ({ color, size }) => {
-              return <Feather name="help-circle" color={color} size={size} />;
-            },
-          }}
-        />
-      </Tab.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Sobre" component={Sobre} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
