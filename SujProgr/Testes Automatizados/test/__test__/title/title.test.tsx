93d49db709c0bc9f9a5061870169484693a3db65
Silpharny <silpharny@gmail.com>
2024-08-06 18:03:11 -0300

    testando componentes

--- SujProgr/Testes Automatizados/test/__test__/title/title.test.tsx
+++ SujProgr/Testes Automatizados/test/__test__/title/title.test.tsx
@@ -2,10 +2,23 @@ import { render } from "@testing-library/react-native"
 
 import { Title } from "../../src/title"
 
-describe("testing title component", () => {
-  it("shoud render title correctly", () => {
+describe("Tests title component", () => {
+  it("should render title correctly", () => {
     const { getByText } = render(<Title title="Sil Miranda" />)
 
     expect(getByText("Sil Miranda")).toBeTruthy()
+  })
+
+  it("should check style", () => {
+    const { getByText } = render(<Title title="Sil Miranda" />)
+
+    const titleElement = getByText("Sil Miranda")
+
+    expect(titleElement.props.style).toMatchObject({
+      fontSize: 24,
+      marginTop: 14,
+      fontWeight: "bold",
+      color: "red",
+    })
   })
 })
