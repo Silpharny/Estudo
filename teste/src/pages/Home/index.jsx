1765fdfcd9c410b1292b61e9cd67b7867a204091
Silpharny <silpharny@gmail.com>
2024-08-17 20:14:20 -0300

    add teste

---
+++ teste/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import { Pressable, Text, View } from "react-native"
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Pressable>
+        <Text>Ir para sobre</Text>
+      </Pressable>
+    </View>
+  )
+}
---
+++ teste/src/pages/Sobre/index.jsx
@@ -0,0 +1,12 @@
+import { Pressable, Text, View } from "react-native"
+
+export default function Sobre() {
+  return (
+    <View>
+      <Text>Sobre</Text>
+      <Pressable>
+        <Text>Ir para Home</Text>
+      </Pressable>
+    </View>
+  )
+}
