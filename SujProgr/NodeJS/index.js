56211a38b72c8a0adc252799577d5ad3b6e05bee
Silpharny <silpharny@gmail.com>
2024-08-21 16:58:28 -0300

    Finalizando módulo NodeJS

--- SujProgr/NodeJS/index.js
+++ SujProgr/NodeJS/index.js
@@ -2,9 +2,69 @@ const express = require("express")
 
 const server = express()
 
-server.get("/", (req, res) => {
-  return res.send({
-    curso: "NodeJS",
-  })
-})
+server.use(express.json())
+
+// Query Params - ?nome=NodeJS
+// Route Params - /curso/1
+// Request Body - { "name": "NodeJS", "type": "Backend" }
+
 server.listen(3000)
+
+const cursos = ["NodeJS", "ReactJS", "React Native"]
+
+// Middlewares
+server.use((req, res, next) => {
+  console.time("Request")
+  console.log(`Método: ${req.method}; URL: ${req.url}`)
+  next()
+  console.timeEnd("Request")
+})
+
+function checkCurso(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "Nome do curso é obrigatório" })
+  }
+  return next()
+}
+
+function checkIndex(req, res, next) {
+  const { index } = req.params
+  if (!cursos[index]) {
+    return res.status(400).json({ error: "Curso inexistente" })
+  }
+  return next()
+}
+
+// POST - Cria uma informação no back-end
+server.post("/cursos", checkCurso, (req, res) => {
+  const { name } = req.body
+  cursos.push(name)
+  return res.json(cursos)
+})
+
+//  GET - Buscar todas informações no back-end
+server.get("/cursos", (req, res) => {
+  return res.json(cursos)
+})
+
+// GET - Buscar uma informação no back-end
+server.get("/cursos/:index", checkIndex, (req, res) => {
+  const { index } = req.params
+
+  return res.json(cursos[index])
+})
+
+// PUT - Atualizar uma informação no back-end
+server.put("/cursos/:index", (req, res) => {
+  const { index } = req.params
+  const { name } = req.body
+  cursos[index] = name
+  return res.json(cursos)
+})
+
+// DELETE - Deletar uma informação no back-end
+server.delete("/cursos/:index", (req, res) => {
+  const { index } = req.params
+  cursos.splice(index, 1)
+  return res.json(cursos)
+})
