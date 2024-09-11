6cc792ba2e12d65656f6311ea925f0810e0531e1
silpharny <silpharny@gmail.com>
2024-09-11 12:49:18 -0300

    calendar

--- shadcnui/src/app/page.tsx
+++ shadcnui/src/app/page.tsx
@@ -1,18 +1,5 @@
-"use client"
-import { Calendar } from "@/components/ui/calendar"
-import { useState } from "react"
-
 export default function Home() {
-  const [date, setDate] = useState<Date | undefined>(new Date())
-
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border"
-      />
-    </div>
+    <div className="flex h-screen w-screen items-center justify-center"></div>
   )
 }
